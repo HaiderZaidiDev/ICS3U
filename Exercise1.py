b1411f39ac5e65a007e5a0a1a1de23c6240a6704
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-10-10 16:26:20 -0400

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,20 @@
+#--------------------------------------------------
+# Name: Exercise1.py
+# Purpose: To practice using basic operations and commands in Python.
+#
+# Author: Haider Zaidi
+# Created: 10/09/2018
+# Updated: 1:02 PM (10/09/2018)
+#---------------------------------------------------
+
+print('Hello World') # print()
+print(6+5) # Addition of integers +
+print(24.3 - 6.2) # Subtraction of float values - 
+print(34.5*2.6) # Multiple of float values *
+print(50/2) # Division of integers /
+print(6//4) # Using integer division //
+print(10%3) # Modulo using integers %
+print(int(9/4)) # Converting between floats and integers int() and float()
+print((6*3)+24/6-10//3%2) # Operators occur in the following order: Brackets, Division, Integer Division, Multipulcation, Modulo, Addition, Substraction. 
+                          # Source: http://www.mathcs.emory.edu/~valerie/courses/fall10/155/resources/op_precedence.html
+
